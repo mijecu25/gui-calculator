86596aa3277b584e374cf9664d552dfce0f8d0fd
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-03 21:16:58 -0600

    11/3/2014 Constructor

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+
+# Main class
+class calculator():
+    # Constructor
+    def __init__(self):
+        # Running total
+        self.total = 0
+        # Input from the user
+        self.input = ""
+        
