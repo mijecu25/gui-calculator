45b27306e6b4c8822fbd797a87f78f19055fc51c
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-03 21:37:31 -0600

    11/3/2014 Working on the window

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,8 +1,15 @@
 from Tkinter import *
 
-#calc = calculator()
+# Creating the window for the calculator
 window = Tk()
+# Creating the canvas
 canvas = Canvas(window, width = 515, height = 515)
+# Organizes widgets in a table-like structure in the parent widget.
 canvas.grid(row = 0, column = 0)
+# Bring the application up to date by invoking idel callbacks
 canvas.update_idletasks()
-window
+# Set title
+window.title("Calculator")
+# Avoid resizing the window
+window.resizable(width = False, height = False)
+#calc = calculator()
