5fea48efeae5fb4bca064e626c3fd6578ea6d019
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-04 20:11:18 -0600

    11/4/2014 Entry text

--- gui_calculator.py
+++ gui_calculator.py
@@ -5,7 +5,7 @@ window = Tk()
 # Creating the canvas
 canvas = Canvas(window, width = 515, height = 515)
 # Organizes widgets in a table-like structure in the parent widget.
-canvas.grid(row = 0, column = 0)
+canvas.grid(row = 4, column = 4)
 # Bring the application up to date by invoking idel callbacks
 canvas.update_idletasks()
 # Set title
@@ -13,3 +13,6 @@ window.title("Calculator")
 # Avoid resizing the window
 window.resizable(width = False, height = False)
 #calc = calculator()
+
+calc_input = Entry(canvas, width = 30, justify = RIGHT)
+calc_input.pack()
