0798ca72206e3a40d172aefa3e18c88a14361752
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-05 09:59:43 -0600

    Create calculator.py

---
+++ calculator/calculator.py
@@ -0,0 +1,8 @@
+# Main class
+class calculator():
+    # Constructor
+    def __init__(self):
+        # Running total
+        self.total = 0
+        # Input from the user
+        self.input = ""
